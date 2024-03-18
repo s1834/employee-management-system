b865e50c6c806212f4b6de6807e288d0c436b91e
s1834 <s18324@protonmail.com>
2024-03-18 05:30:43 +0530

    Category design, api and display

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
+import Home from './components/Home';
+import Employee from './components/Employee';
+import Category from './components/Category';
+import Profile from './components/Profile';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import AddCategory from './components/AddCategory';
 
 function App() {
   return (
@@ -10,7 +15,16 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/adminlogin" element={<Login />}></Route>
-          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}>
+            <Route path="" element={<Home />}></Route>
+            <Route path="/dashboard/employee" element={<Employee />}></Route>
+            <Route path="/dashboard/category" element={<Category />}></Route>
+            <Route path="/dashboard/profile" element={<Profile />}></Route>
+            <Route
+              path="/dashboard/add_category"
+              element={<AddCategory />}
+            ></Route>
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
